a6ea53881f927c4253bcc9636769e6f6cfa292ba
Aslam-dev <almaslam01@gmail.com>
2023-12-18 21:29:10 +0530

    layout desing

---
+++ frontend/src/components/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "../../styles/styles";
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/Layout/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { navItems } from '../../static/data'
+import styles from '../../styles/styles'
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
